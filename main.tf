c5c10b7e2776e08479052a9c7f6ae1cb17f97325
Eric Duquesnoy <16538036+ded703@users.noreply.github.com>
2022-03-31 11:19:06 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,14 @@ provider "azurerm" {
 }
 
 #ajout d'une ressource
-resource "azurerm_resource_group" "terraform-cloud" {
+resource "azurerm_resource_group" "rg" {
   name     = "TERRAFORM_ESSA"
   location = "francecentral"
+}
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+    name                = "myTFVnet"
+    address_space       = ["10.1.0.0/24"]
+    location            = "francecentral"
+    resource_group_name = azurerm_resource_group.rg.name
 }
